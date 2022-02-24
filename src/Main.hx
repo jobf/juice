9a4c1c644c172f4b1a8de438acc0e12675d80171
jf <j@b.f>
2022-02-24 00:54:28 +0000

    test all waves

--- src/Main.hx
+++ src/Main.hx
@@ -7,21 +7,30 @@ import ob.juice.Wave.WaveFile;
 /** here we are testing the tone generation by writing the wav to disk **/
 class Main {
 	static public function main() {
-		var tone = new Tone(660, {
+		var envShape = {
 			a: 0.5,
 			d: 0.5,
 			s: 0.5,
 			r: 0.5
-		});
-		var toneBuffer = new ToneBuffer(tone, 2.0);
-		toneBuffer.bufferSamples();
+		};
 
-		var fileOutput = File.write('tone.wav', true);
-		WaveFile.write(toneBuffer.sampleData, fileOutput);
+		var frequency = 500;
+		var noteLength = 2.0;
 
-		fileOutput.close();
+		for(waveShape in [SINE, TRI, PULSE, SAW]){
+			var tone = new Tone(frequency, envShape, waveShape);
 
-		trace('wrote file');
+			var toneBuffer = new ToneBuffer(tone, noteLength);
+			toneBuffer.bufferSamples();
+
+			var filePath = '$waveShape.wav';
+			var fileOutput = File.write(filePath, true);
+			WaveFile.write(toneBuffer.sampleData, fileOutput);
+
+			fileOutput.close();
+
+			trace('wrote $filePath');
+		};
 	}
 }
 #end
